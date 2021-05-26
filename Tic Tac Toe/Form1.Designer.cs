be904ab2236a217da84732179d7d78601013e8bb
DrSeuss127 <clintcalumpad@gmail.com>
2021-05-26 22:46:24 +0800

    Added various functions and tweaks
    -> Hover mouse to see whose turn it is
    -> Added counter for number of turns left
    -> Tic-Tac-Toe Grid now resets whenever there is a winner, or whenever there is a draw
    -> Added reset score button
    -> Added comments
    -> Cleaned extra lines of code

--- Tic Tac Toe/Form1.Designer.cs
+++ Tic Tac Toe/Form1.Designer.cs
@@ -40,9 +40,12 @@ namespace Tic_Tac_Toe
             this.button9 = new System.Windows.Forms.Button();
             this.p1Label = new System.Windows.Forms.Label();
             this.p2Label = new System.Windows.Forms.Label();
-            this.resetBtn = new System.Windows.Forms.Button();
             this.p1Win = new System.Windows.Forms.Label();
             this.p2Win = new System.Windows.Forms.Label();
+            this.turnLbl = new System.Windows.Forms.Label();
+            this.turnCountLbl = new System.Windows.Forms.Label();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.resetScoreBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -54,6 +57,8 @@ namespace Tic_Tac_Toe
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button_click);
+            this.button1.MouseEnter += new System.EventHandler(this.mouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.mouseLeave);
             // 
             // button2
             // 
@@ -64,6 +69,8 @@ namespace Tic_Tac_Toe
             this.button2.TabIndex = 0;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button_click);
+            this.button2.MouseEnter += new System.EventHandler(this.mouseEnter);
+            this.button2.MouseLeave += new System.EventHandler(this.mouseLeave);
             // 
             // button3
             // 
@@ -74,6 +81,8 @@ namespace Tic_Tac_Toe
             this.button3.TabIndex = 0;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button_click);
+            this.button3.MouseEnter += new System.EventHandler(this.mouseEnter);
+            this.button3.MouseLeave += new System.EventHandler(this.mouseLeave);
             // 
             // button4
             // 
@@ -84,6 +93,8 @@ namespace Tic_Tac_Toe
             this.button4.TabIndex = 0;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button_click);
+            this.button4.MouseEnter += new System.EventHandler(this.mouseEnter);
+            this.button4.MouseLeave += new System.EventHandler(this.mouseLeave);
             // 
             // button5
             // 
@@ -94,6 +105,8 @@ namespace Tic_Tac_Toe
             this.button5.TabIndex = 0;
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button_click);
+            this.button5.MouseEnter += new System.EventHandler(this.mouseEnter);
+            this.button5.MouseLeave += new System.EventHandler(this.mouseLeave);
             // 
             // button6
             // 
@@ -104,6 +117,8 @@ namespace Tic_Tac_Toe
             this.button6.TabIndex = 0;
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button_click);
+            this.button6.MouseEnter += new System.EventHandler(this.mouseEnter);
+            this.button6.MouseLeave += new System.EventHandler(this.mouseLeave);
             // 
             // button7
             // 
@@ -114,6 +129,8 @@ namespace Tic_Tac_Toe
             this.button7.TabIndex = 0;
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button_click);
+            this.button7.MouseEnter += new System.EventHandler(this.mouseEnter);
+            this.button7.MouseLeave += new System.EventHandler(this.mouseLeave);
             // 
             // button8
             // 
@@ -124,6 +141,8 @@ namespace Tic_Tac_Toe
             this.button8.TabIndex = 0;
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.button_click);
+            this.button8.MouseEnter += new System.EventHandler(this.mouseEnter);
+            this.button8.MouseLeave += new System.EventHandler(this.mouseLeave);
             // 
             // button9
             // 
@@ -134,6 +153,8 @@ namespace Tic_Tac_Toe
             this.button9.TabIndex = 0;
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button_click);
+            this.button9.MouseEnter += new System.EventHandler(this.mouseEnter);
+            this.button9.MouseLeave += new System.EventHandler(this.mouseLeave);
             // 
             // p1Label
             // 
@@ -155,21 +176,11 @@ namespace Tic_Tac_Toe
             this.p2Label.TabIndex = 1;
             this.p2Label.Text = "Player 2 (O) Wins :";
             // 
-            // resetBtn
-            // 
-            this.resetBtn.Location = new System.Drawing.Point(610, 320);
-            this.resetBtn.Name = "resetBtn";
-            this.resetBtn.Size = new System.Drawing.Size(75, 54);
-            this.resetBtn.TabIndex = 2;
-            this.resetBtn.Text = "Reset Table";
-            this.resetBtn.UseVisualStyleBackColor = true;
-            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
-            // 
             // p1Win
             // 
             this.p1Win.AutoSize = true;
             this.p1Win.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.p1Win.Location = new System.Drawing.Point(237, 59);
+            this.p1Win.Location = new System.Drawing.Point(235, 61);
             this.p1Win.Name = "p1Win";
             this.p1Win.Size = new System.Drawing.Size(21, 24);
             this.p1Win.TabIndex = 3;
@@ -179,21 +190,64 @@ namespace Tic_Tac_Toe
             // 
             this.p2Win.AutoSize = true;
             this.p2Win.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.p2Win.Location = new System.Drawing.Point(504, 59);
+            this.p2Win.Location = new System.Drawing.Point(499, 60);
             this.p2Win.Name = "p2Win";
             this.p2Win.Size = new System.Drawing.Size(21, 24);
             this.p2Win.TabIndex = 3;
             this.p2Win.Text = "0";
             // 
+            // turnLbl
+            // 
+            this.turnLbl.AutoSize = true;
+            this.turnLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.turnLbl.Location = new System.Drawing.Point(124, 126);
+            this.turnLbl.Name = "turnLbl";
+            this.turnLbl.Size = new System.Drawing.Size(185, 24);
+            this.turnLbl.TabIndex = 1;
+            this.turnLbl.Text = "Player Turn Count:";
+            // 
+            // turnCountLbl
+            // 
+            this.turnCountLbl.AutoSize = true;
+            this.turnCountLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.turnCountLbl.Location = new System.Drawing.Point(308, 127);
+            this.turnCountLbl.Name = "turnCountLbl";
+            this.turnCountLbl.Size = new System.Drawing.Size(21, 24);
+            this.turnCountLbl.TabIndex = 3;
+            this.turnCountLbl.Text = "0";
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(534, 357);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(78, 47);
+            this.resetBtn.TabIndex = 4;
+            this.resetBtn.Text = "Reset Grid";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // resetScoreBtn
+            // 
+            this.resetScoreBtn.Location = new System.Drawing.Point(534, 304);
+            this.resetScoreBtn.Name = "resetScoreBtn";
+            this.resetScoreBtn.Size = new System.Drawing.Size(78, 47);
+            this.resetScoreBtn.TabIndex = 4;
+            this.resetScoreBtn.Text = "Reset Score";
+            this.resetScoreBtn.UseVisualStyleBackColor = true;
+            this.resetScoreBtn.Click += new System.EventHandler(this.resetScoreBtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 652);
-            this.Controls.Add(this.p2Win);
-            this.Controls.Add(this.p1Win);
+            this.ClientSize = new System.Drawing.Size(659, 577);
+            this.Controls.Add(this.resetScoreBtn);
             this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.p2Win);
+            this.Controls.Add(this.turnCountLbl);
+            this.Controls.Add(this.p1Win);
             this.Controls.Add(this.p2Label);
+            this.Controls.Add(this.turnLbl);
             this.Controls.Add(this.p1Label);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
@@ -204,7 +258,9 @@ namespace Tic_Tac_Toe
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.ShowIcon = false;
             this.Text = "Tic Tac Toe";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -224,9 +280,14 @@ namespace Tic_Tac_Toe
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Label p1Label;
         private System.Windows.Forms.Label p2Label;
-        private System.Windows.Forms.Button resetBtn;
         private System.Windows.Forms.Label p1Win;
         private System.Windows.Forms.Label p2Win;
+        private System.Windows.Forms.Label turnLbl;
+        private System.Windows.Forms.Label turnCountLbl;
+        private System.Windows.Forms.Button resetScore;
+        private System.Windows.Forms.Button gridReset;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Button resetScoreBtn;
     }
 }
 
--- Tic Tac Toe/Form1.cs
+++ Tic Tac Toe/Form1.cs
@@ -12,6 +12,8 @@ namespace Tic_Tac_Toe
 {
     public partial class Form1 : Form
     {
+
+        //Variables for the players' turns, the number of turns left, and the counter for p1 and p2 scores
         bool playerTurn = true;
         int playerTurnCount = 0;
         int p1WinCount = 0;
@@ -24,9 +26,9 @@ namespace Tic_Tac_Toe
 
         private void button_click(object sender, EventArgs e) //Single event handler for all tic-tac-toe-grid buttons
         {
-            Button b = (Button)sender; //Declares variable b as the button sender, which applies to the tic-tac-toe 3x3 grid
+            Button b = (Button)sender;                      //Declares variable b as button and converts the object "sender" to a button
 
-            if (playerTurn)             //Playerturn is true by default, !playerturn negates it
+            if (playerTurn)                                 //playerTurn is true by default, !playerturn negates it
             {
                 b.Text = "X";
             }
@@ -34,16 +36,18 @@ namespace Tic_Tac_Toe
             {
                 b.Text = "O";
             }
-            playerTurn = !playerTurn; //Negates playerturn
-            b.Enabled = false;          //Disables the button once a player has clicked
+
+            playerTurn = !playerTurn;                       //Negates playerTurn
+            b.Enabled = false;                              //Disables the button once a player has clicked
+            playerTurnCount++;                              //Increments players' number of turns left whenever a button is clicked
+            turnCountLbl.Text = playerTurnCount.ToString(); //Writes the number of turns to the target label
 
 
-            //Checks every possible combination of the same 3 characters in the tic tac toe grid
-            //It does this by checking if the text in all 3 buttons are the same, and if the first button in the row/column/diagonal is not enabled.
+            //Checks every possible combination of the same 3 characters in the tic tac toe grid and if first button is disabled to avoid message box showing earlier than expected
             bool thereIsAWinner = false;
 
-            //Horizontal Checking
-            if ((button1.Text == button4.Text) && (button4.Text == button7.Text) && (!button1.Enabled))
+            //Horizontal Checking -------
+            if ((button1.Text == button4.Text) && (button4.Text == button7.Text) && (!button1.Enabled)) 
             {
                 thereIsAWinner = true;
             }
@@ -56,7 +60,8 @@ namespace Tic_Tac_Toe
                 thereIsAWinner = true;
             }
 
-            //Vertical Checking
+
+            //Vertical Checking ||||||||
             else if ((button1.Text == button2.Text) && (button2.Text == button3.Text) && (!button1.Enabled))
             {
                 thereIsAWinner = true;
@@ -70,7 +75,8 @@ namespace Tic_Tac_Toe
                 thereIsAWinner = true;
             }
 
-            //Diagonal Checking
+
+            //Diagonal Checking \\\\////
             else if ((button1.Text == button5.Text) && (button5.Text == button9.Text) && (!button1.Enabled))
             {
                 thereIsAWinner = true;
@@ -83,6 +89,7 @@ namespace Tic_Tac_Toe
 
             if (thereIsAWinner)
             {
+                //Disables all the buttons once there is a winner
                 button1.Enabled = false;
                 button2.Enabled = false;
                 button3.Enabled = false;
@@ -93,33 +100,147 @@ namespace Tic_Tac_Toe
                 button8.Enabled = false;
                 button9.Enabled = false;
 
-                //Stores the string of the winner
+                //Stores the string value of the winner (X or O)
                 string winner = "";
-                
 
+
+                //Clears the output for number of turns
+                turnCountLbl.Text = "0";                
+
+
+                //Determines which of the two players won and increments their score by 1
                 if (playerTurn)
                 {
+                    playerTurn = true;                  //Resets whose turn it is
+                    playerTurnCount = 0;                //Resets the number of turns left
+
                     p2WinCount ++;
                     winner = "O";
                     p2Win.Text = "";
                     p2Win.Text = p2WinCount.ToString();
-                        ;
+
+                    MessageBox.Show("Player 2 (" + winner + ") wins!");
+
+                    //Resets all buttons and enables them again after a winner is identified
+                    button1.Text = "";
+                    button1.Enabled = true;
+
+                    button2.Text = "";
+                    button2.Enabled = true;
+
+                    button3.Text = "";
+                    button3.Enabled = true;
+
+                    button4.Text = "";
+                    button4.Enabled = true;
+
+                    button5.Text = "";
+                    button5.Enabled = true;
+
+                    button6.Text = "";
+                    button6.Enabled = true;
+
+                    button7.Text = "";
+                    button7.Enabled = true;
+
+                    button8.Text = "";
+                    button8.Enabled = true;
+
+                    button9.Text = "";
+                    button9.Enabled = true;
+
                 }
                 else
                 {
+                    playerTurn = true;                  //Resets whose turn it is
+                    playerTurnCount = 0;                //Resets number of turns left
+
                     p1WinCount ++;
                     winner = "X";
                     p1Win.Text = "";
-                    p1Win.Text = p1WinCount.ToString(); 
-                }
+                    p1Win.Text = p1WinCount.ToString();
 
-                MessageBox.Show(winner + " wins!");
+                    MessageBox.Show("Player 1 (" + winner + ") wins!");
+
+
+                    //Resets all buttons and enables them again after a winner is identified
+                    button1.Text = "";
+                    button1.Enabled = true;
+
+                    button2.Text = "";
+                    button2.Enabled = true;
+
+                    button3.Text = "";
+                    button3.Enabled = true;
+
+                    button4.Text = "";
+                    button4.Enabled = true;
+
+                    button5.Text = "";
+                    button5.Enabled = true;
+
+                    button6.Text = "";
+                    button6.Enabled = true;
+
+                    button7.Text = "";
+                    button7.Enabled = true;
+
+                    button8.Text = "";
+                    button8.Enabled = true;
+
+                    button9.Text = "";
+                    button9.Enabled = true;
+                }
+            }
+            else
+            {
+                if (playerTurnCount == 9)               //Checks if the number of turns is equal to 9. If it is, shows a message box and resets the buttons
+                {
+                    MessageBox.Show("There is no winner. Resetting the grid..");
+
+                    playerTurn = true;                  //Resets whose turn it is
+                    playerTurnCount = 0;                //Resets the number of turns
+
+                    turnCountLbl.Text = "0";            //Clears the output for number of turns
+
+                    button1.Text = "";
+                    button1.Enabled = true;
+
+                    button2.Text = "";
+                    button2.Enabled = true;
+
+                    button3.Text = "";
+                    button3.Enabled = true;
+
+                    button4.Text = "";
+                    button4.Enabled = true;
+
+                    button5.Text = "";
+                    button5.Enabled = true;
+
+                    button6.Text = "";
+                    button6.Enabled = true;
+
+                    button7.Text = "";
+                    button7.Enabled = true;
+
+                    button8.Text = "";
+                    button8.Enabled = true;
+
+                    button9.Text = "";
+                    button9.Enabled = true;
+
+                }
             }
         }
 
-        private void resetBtn_Click(object sender, EventArgs e) 
+        private void resetBtn_Click(object sender, EventArgs e)             //Event handler for a manual reset grid button
         {
             //Resets the table; empties the button texts and enables the buttons
+
+            playerTurn = true;          //Resets whose turn it is
+            playerTurnCount = 0;        //Resets the number of turns
+            turnCountLbl.Text = "0";    //Clears the output for number of turns
 
             button1.Text = "";
             button1.Enabled = true;
@@ -147,6 +268,45 @@ namespace Tic_Tac_Toe
 
             button9.Text = "";
             button9.Enabled = true;
+        }
+
+        private void resetScoreBtn_Click(object sender, EventArgs e)     //Event handler for a manual reset score button
+        {
+            //Resets values for the wins count for both players
+            p1WinCount = 0;
+            p2WinCount = 0;
+
+            //Clears the values on the output label
+            p1Win.Text = "0";
+            p2Win.Text = "0";
+        }
+
+        private void mouseEnter(object sender, EventArgs e)             //Event handler for determining whose turn it is when hovering over the button
+        {
+
+            Button b = (Button)sender;  
+
+            if (b.Enabled)
+            {
+                if (playerTurn)
+                {
+                    b.Text = "X";
+                }
+                else
+                {
+                    b.Text = "O";
+                }
+            }
+        }
+
+        private void mouseLeave(object sender, EventArgs e)         //Event handler for clearing the button text whenever the mouse leaves the button
+        {
+            Button b = (Button)sender;
+
+            if (b.Enabled)
+            {
+                b.Text = "";
+            }
         }
     }
 }
